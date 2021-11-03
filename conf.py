37313e26e6f7bca09011ffa84e63054e504e9bb6
Willem Essenstam <wessenstam@gmail.com>
2021-11-03 13:40:34 -0700

    Updated test version

--- conf.py
+++ conf.py
@@ -94,9 +94,9 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # (Optional) Logo. Should be small enough to fit the navbar (ideally 24x24).
 # Path should be relative to the ``_static`` files directory.
 
-html_logo = "ThycoticWorkshops.svg"
+html_logo = "_static/ThycoticWorkshops.svg"
 
-html_favicon = "favicon.ico"
+html_favicon = "_static/favicon.ico"
 
 html_title = ""
 
